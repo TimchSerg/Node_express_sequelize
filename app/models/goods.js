9f4f413bafc0f111f9feca77ed30cd740be78d53
Serg <sergey.timoshenko.iv@gmail.com>
2020-07-01 11:29:07 +0300

    add file and gitignore

---
+++ app/models/goods.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Goods extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Goods.init({
+    name: DataTypes.STRING,
+    price: DataTypes.FLOAT,
+    count: DataTypes.FLOAT
+  }, {
+    sequelize,
+    modelName: 'Goods',
+  });
+  return Goods;
+};
